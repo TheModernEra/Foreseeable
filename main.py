0336ee37488e4a442280c7803276d1484868f830
TheModernEra <bsrosenthal101@gmail.com>
2021-04-10 01:07:59 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+import requests
+import pathlib
+import json
+
+r = requests.get('http://api.weatherstack.com/current?access_key=e682fc6afa695092cdeb579da6354f04&units=f&query=Woodland Hills, California')
+pathlib.Path('data.json').write_bytes(r.content)
+
+
