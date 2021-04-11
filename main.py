c3d13d1e1d8a6bbc84a23ff3bdf04853f5981c67
TheModernEra <bsrosenthal101@gmail.com>
2021-04-11 00:07:32 -0700

    finished i guess

--- main.py
+++ main.py
@@ -13,21 +13,30 @@ from PyQt5.QtGui import *
 import requests
 import pathlib
 import json
+import urllib
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Foreseeable")
+        self.setWindowIcon(QIcon("icon.png"))
+        self.setFixedWidth(700)
+        self.setFixedHeight(100)
         # Create a QHBoxLayout instance
         self.layout = QHBoxLayout()
         # Add widgets to the layout
         self.lineEdit = QLineEdit()
         self.layout.addWidget(self.lineEdit)
-        self.label = QLabel("Enter a City (and a State optionally)")
-        self.layout.addWidget(self.label)
         self.button = QPushButton("Get Temp!")
         self.button.clicked.connect(self.button_click)
         self.layout.addWidget(self.button)
+        self.label = QLabel("Enter a City (and a State optionally)")
+        self.layout.addWidget(self.label)
+        self.image = QLabel()
+        question = QPixmap('question.jpg')
+        question = question.scaledToWidth(75)
+        self.image.setPixmap(question)
+        self.layout.addWidget(self.image)
         # Set the layout on the application's window
         self.setLayout(self.layout)
         print(self.children())
@@ -40,6 +49,11 @@ class Window(QWidget):
         with open('data.json') as f:
             data = json.load(f)
         self.label.setText("The current temperature in "+ query + " is " + str(data["current"]["temperature"]) + " degrees")
+        imgurl = data["current"]["weather_icons"][0]
+        imgdata = urllib.request.urlopen(imgurl).read()
+        imagePixmap = QPixmap()
+        imagePixmap.loadFromData(imgdata)
+        self.image.setPixmap(imagePixmap)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
